d8197165e98bdf74b70851135f647b886b178860
Ömer Türkgeldi <turkgeldiomer@gmail.com>
2021-12-09 17:59:30 +0300

    Bootstrap cards eklendi

--- src/views/pages/Daily.js
+++ src/views/pages/Daily.js
@@ -1,14 +1,90 @@
-import React from 'react'
+import React from "react";
 
 const Daily = () => {
-    return (
-        <div>
-            <h1>Günlük</h1>
-            <h1>Günlük</h1>
-            <h1>Günlük</h1>
-            <h1>Günlük</h1>
+  return (
+    <div className="container">
+      <div className="mt-3 row">
+        <div className="col-md-6">
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="col-md-6">
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-3 row">
+        <div className="col-md-6">
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1>Günlük</h1>
+      <h1>Günlük</h1>
+      <h1>Günlük</h1>
+      <h1>Günlük</h1>
+    </div>
+  );
+};
 
-export default Daily
+export default Daily;
