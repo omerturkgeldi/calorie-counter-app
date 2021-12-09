effa8c43986611ffde2911326e31facff8857209
Ömer Türkgeldi <turkgeldiomer@gmail.com>
2021-12-09 17:37:10 +0300

    Views klasörü güncellendi.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 // import Dashboard from '../pages/Dashboard'
-import Foods from "./views/Foods/Foods";
+import Foods from "./views/pages/Foods/Foods";
 import Products from "./views/Products/Products";
-import Dashboard from "./views/Dashboard/Dashboard";
-import Daily from "./views/Daily/Daily";
-import Activities from "./views/Activities/Activities";
-import Social from "./views/Social/Social";
+import Dashboard from "./views/pages/Dashboard/Dashboard";
+import Daily from "./views/pages/Daily/Daily";
+import Activities from "./views/pages/Activities/Activities";
+import Social from "./views/pages/Social/Social";
 
 const Routes = () => {
   return (
